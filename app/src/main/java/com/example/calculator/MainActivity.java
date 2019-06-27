cb5bdb01f9887bade7334c1ed9ff063cf0500d68
jacob andersen <jacobma87@hotmail.com>
2019-06-27 14:05:03 -0400

    Initial commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,123 @@
+package com.example.calculator;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_add, btn_sub, btn_mult, btn_div, btn1, btn2,btn3, btn4, btn5, btn6, btn7,btn8,btn9,btn0,btn_clr, btn_equal;
+    TextView display;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity2);
+
+        btn_add = findViewById(R.id.button1);
+        btn_sub = findViewById(R.id.button2);
+        btn_mult = findViewById(R.id.button3);
+        btn_div = findViewById(R.id.button4);
+        btn1 = findViewById(R.id.button5);
+        btn2 = findViewById(R.id.button6);
+        btn3 = findViewById(R.id.button7);
+        btn4 = findViewById(R.id.button8);
+        btn5 = findViewById(R.id.button9);
+        btn6 = findViewById(R.id.button10);
+        btn7 = findViewById(R.id.button11);
+        btn8 = findViewById(R.id.button12);
+        btn9 = findViewById(R.id.button13);
+        btn0 = findViewById(R.id.button14);
+        btn_clr = findViewById(R.id.button15);
+        btn_equal = findViewById(R.id.button16);
+        display = findViewById(R.id.textview_display);
+
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                display.append("+");
+            }
+        });
+        btn_sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                display.append("-");
+            }
+        });
+        btn_div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                display.append("/");
+            }
+        });
+        btn_mult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                display.append("*");
+            }
+        });
+        btn_equal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                display.append("=");
+            }
+        });
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                display.append("1");
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                display.append("2");
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                display.append("3");
+            }
+        });btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                display.append("4");
+            }
+        });btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                display.append("5");
+            }
+        });btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                display.append("6");
+            }
+        });btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                display.append("7");
+            }
+        });btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                display.append("8");
+            }
+        });btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                display.append("9");
+            }
+        });btn0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                display.append("0");
+            }
+        });
+    }
+}
